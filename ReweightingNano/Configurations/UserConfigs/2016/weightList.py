b86daa45b45830d3b17c3f6f7bfc6d825adc5dfd
gparida <ganeshsanu111@gmail.com>
2022-06-08 10:56:36 +0200

    Update weightList.py

--- ReweightingNano/Configurations/UserConfigs/2016/weightList.py
+++ ReweightingNano/Configurations/UserConfigs/2016/weightList.py
@@ -1,7 +1,7 @@
 from Configurations.ConfigDefinition import ReweightConfiguration
 from Configurations.Weights.CrossSectionWeightingModule.CrossSectionWeight import crossSectionWeight as crossSectionWeight
 from Configurations.Weights.pileupWeightingModule.pileupWeight import pileupWeight_2016
-from Configurations.Weights.TauIDModule.TauIDWeight import tauIDWeight_2016 as tauIDWeight
+#from Configurations.Weights.TauIDModule.TauIDWeight import tauIDWeight_2016 as tauIDWeight
 from Configurations.Weights.TriggerSFModule.triggerWeight import triggerWeight_2016
 from Configurations.Weights.BtagWeightingModule.bTagweighting import btagWeight_2016
 
