1c33faeb770ec8de08fd6174e3beb0a883372695
gparida <ganeshsanu111@gmail.com>
2022-06-18 16:51:10 +0200

    Update the json file creator - to dump the json in a location other than current directory

--- MetaData/scriptToCreateJsonForReweightingInput.py
+++ MetaData/scriptToCreateJsonForReweightingInput.py
@@ -2,13 +2,21 @@
 from pathlib import Path
 import argparse
 import json
+import os
 
 parser = argparse.ArgumentParser(description='A script to create the skeleton of the samples JSON file which has the dataset name key. It also has the path to the file and keys for XS and Kfactor lumi etc')
 parser.add_argument('-b', '--basePath',help="Enter base path of Location of Root Files",required=True)
-parser.add_argument('-s','--savePath',help="where to dump the json file along with the name")
+parser.add_argument('-s','--savePath',help="where to dump the json file")
+parser.add_argument('-n','--name',help="name of  the json file with .json at the end")
 #parser.add_argument('--haddDir',help="Folder in which the files are present",required=True)
 #parser.add_argument('--savePath',help="Place where to store the hadded files",required=True)
 args = parser.parse_args()
+
+
+filepath = os.path.join(args.savePath, args.name)
+
+if not os.path.exists(args.savePath):
+    os.makedirs(args.savePath)
 
 
 
@@ -27,7 +35,7 @@ for key in keyname:
 
 print (finalDict)
 
-with open(args.savePath, 'w') as writeFile:
+with open(filepath, 'w') as writeFile:
         json.dump(finalDict, writeFile, indent=4)
 
 
