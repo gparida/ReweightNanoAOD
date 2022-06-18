6c9eccd11ef7ca5a994c8289f31017860e5c8cbb
gparida <ganeshsanu111@gmail.com>
2022-06-18 18:22:05 +0200

    Update makeUserFilesConfig.py

--- ReweightingNano/Configurations/makeUserFilesConfig.py
+++ ReweightingNano/Configurations/makeUserFilesConfig.py
@@ -1,6 +1,12 @@
 import json
 import os
+import argparse
 
+parser = argparse.ArgumentParser(description='This is create configuration files for different samples fora time period')
+parser.add_argument('-n','--name',help="name of  the json file with .json at the end where from we will read the file location in this area ReweightNanoAOD/MetaData ex 2016_Samples.json")
+parser.add_argument('-y','--year',help="so that we know which year folder we need to put the configs")
+ 
+args = parser.parse_args()
 
 string_out = """
 import ROOT
@@ -16,7 +22,7 @@ from Configurations.Weights.CrossSectionWeightingModule.CrossSectionWeight impor
 {refinedName}Config = ReweightConfiguration()
 {refinedName}Config.name = '{Name}'
 #QCD_Pt_15to30Config.jsonSampleFile = os.environ['CMSSW_BASE']+'/src/bbtautauAnalysisScripts/analysisCore/config/samples/2016_Samples.json'
-{refinedName}Config.jsonSampleFile = os.environ['CMSSW_BASE']+'/src/ReweightNanoAOD/MetaData/2016_Samples_25Jan.json'
+{refinedName}Config.jsonSampleFile = os.environ['CMSSW_BASE']+'/src/ReweightNanoAOD/MetaData/{jsonName}'
 
 with open({refinedName}Config.jsonSampleFile,'r') as jsonFile:
     jsonInfo = json.load(jsonFile)
@@ -42,14 +48,58 @@ except KeyError:
 {refinedName}Config.listOfWeights = list
 """
 
-with open(os.environ['CMSSW_BASE']+'/src/ReweightNanoAOD/MetaData/2016_Samples_25Jan.json') as jsonFile:
-    data = json.load(jsonFile)
+string_out_data = """
+import ROOT
+import os
+import json
+from weightList import *
 
-keys = data.keys()
+from Configurations.ConfigDefinition import ReweightConfiguration
+from Configurations.Weights.CrossSectionWeightingModule.CrossSectionWeight import crossSectionWeight as crossSectionWeight
+#from Configurations.Weights.pileupWeightingModule.pileupWeight import pileupWeight_2016
+
+
+{refinedName}Config = ReweightConfiguration()
+{refinedName}Config.name = '{Name}'
+#QCD_Pt_15to30Config.jsonSampleFile = os.environ['CMSSW_BASE']+'/src/bbtautauAnalysisScripts/analysisCore/config/samples/2016_Samples.json'
+{refinedName}Config.jsonSampleFile = os.environ['CMSSW_BASE']+'/src/ReweightNanoAOD/MetaData/{jsonName}'
+
+with open({refinedName}Config.jsonSampleFile,'r') as jsonFile:
+    jsonInfo = json.load(jsonFile)
+theFile = ROOT.TFile(jsonInfo[{refinedName}Config.name]['file'])
+totalNumberOfEvents = theFile.cutflow.GetBinContent(1)
+theFile.Close()
+
+{refinedName}Config.inputFile = jsonInfo[{refinedName}Config.name]['file']
+{refinedName}Config.inputFile_tt = jsonInfo[{refinedName}Config.name]['file_tt']
+{refinedName}Config.inputFile_et = jsonInfo[{refinedName}Config.name]['file_et']
+{refinedName}Config.inputFile_mt = jsonInfo[{refinedName}Config.name]['file_mt']
+
+
+crossSectionWeight.XS = jsonInfo[{refinedName}Config.name]['XS'] * 1e-12 #XS in pb
+crossSectionWeight.timePeriod = '2016'
+crossSectionWeight.totalNumberOfEvents = totalNumberOfEvents
+try:
+    crossSectionWeight.forcedGenWeight = jsonInfo[{refinedName}Config.name]['forcedGenWeight']
+except KeyError:
+    crossSectionWeight.forcedGenWeight = None
+
+
+{refinedName}Config.listOfWeights = []
+"""
+
+with open(os.environ['CMSSW_BASE']+'/src/ReweightNanoAOD/MetaData/' +args.name) as jsonFile:
+    entries = json.load(jsonFile)
+
+keys = entries.keys()
 #save_path = '/UserConfigs/2016'
 
 for name in keys:
-    with open('UserConfigs/2016/'+name.replace("-","_")+'Config.py','w+') as file_out:
+    if  name == "Data":
+        with open('UserConfigs/'+args.year+'/'+name.replace("-","_")+'Config.py','w+') as file_out:
+            file_out.write(string_out_data.format(refinedName=name.replace("-","_"),Name=name))
+
+    with open('UserConfigs/'+args.year+'/'+name.replace("-","_")+'Config.py','w+') as file_out:
         file_out.write(string_out.format(refinedName=name.replace("-","_"),Name=name))
 
 
