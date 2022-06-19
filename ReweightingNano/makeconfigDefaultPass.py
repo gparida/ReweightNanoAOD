dd8b5fd47a88b31e4dc4c186889d56567c395f1b
gparida <ganeshsanu111@gmail.com>
2022-06-19 14:05:52 +0200

    Update makeconfigDefaultPass.py

--- ReweightingNano/makeconfigDefaultPass.py
+++ ReweightingNano/makeconfigDefaultPass.py
@@ -21,7 +21,13 @@ config_names=[files.as_posix() for files in list(config_path.glob("*Config*.py")
 
 with open('configDefaultPass_'+args.year+".py", 'w+') as fp:
     fp.write('configList=[\n')
-    fp.write('",\n'.join(config_names))
-    fp.write('\n]')
+    for count,na in config_names:
+        if count != len(config_names)-1:
+            fp.write('"'+na+'",\n')
+        else:
+            fp.write('"'+na+'"\n')
+
+    
+    fp.write(']')
 
 print (config_names)
