0ec69a77a39dd86aa828d5920d3df31d36e5428c
gparida <ganeshsanu111@gmail.com>
2022-06-19 13:42:12 +0200

    Update makeconfigDefaultPass.py

--- ReweightingNano/makeconfigDefaultPass.py
+++ ReweightingNano/makeconfigDefaultPass.py
@@ -12,8 +12,8 @@ args = parser.parse_args()
 config_path = Path("Configurations/UserConfigs/"+args.year+"/")
 config_dict = dict()
 
-config_names=["Configurations/UserConfigs/"+args.year+"/"+files.name[:] for files in list(config_path.glob("*Config*.py"))]
+#config_names=["Configurations/UserConfigs/"+args.year+"/"+files.name[:] for files in list(config_path.glob("*Config*.py"))]
 
-config_names=[files for files in list(config_path.glob("*Config*.py"))]
+config_names=[files.as_posix() for files in list(config_path.glob("*Config*.py"))]
 
 print (config_names)
