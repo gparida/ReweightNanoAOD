d2603cd7e3b164d3582b962a4340e5149ff9b3e4
gparida <ganeshsanu111@gmail.com>
2022-06-19 13:32:01 +0200

    Create makeconfigDefaultPass.py

---
+++ ReweightingNano/makeconfigDefaultPass.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import argparse
+import json
+import os
+
+
+parser = argparse.ArgumentParser(description='Script to store the path to all the user configs for a specifc year and create a defaulConfigPass')
+parser.add_argument('-y','--year',help="so that we know which year folder we need to put the configs")
+args = parser.parse_args()
+
+config_path = Path("Configurations/UserConfigs/"+args.year+"/")
+config_dict = dict()
+
+config_names=[files.name[:] for files in list(config_path.glob("*Config*.py"))]
+
+print (config_names)
